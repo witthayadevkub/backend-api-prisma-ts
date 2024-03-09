abc8039980bf2384ee0e6cd941f8d1935abb2173
witthayadevkub <tigermixdev@gmail.com>
2024-03-09 12:25:48 +0700

    test

--- router/auth.ts
+++ router/auth.ts
@@ -1,7 +1,6 @@
 const express = require("express");
 import { PrismaClient } from '@prisma/client';
 import type { Request, Response } from 'express';
-
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const prisma = new PrismaClient()
